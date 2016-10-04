4b4ed8f9cef6e5621c5a7b8ede3c40dbfc44d696
Roberto Allende <rallende@gmail.com>
2016-10-04 02:53:15 -0300

    Adding test cases.

--- app/src/androidTest/java/com/robertoallende/marvelcomics/ApplicationTest.java
+++ app/src/androidTest/java/com/robertoallende/marvelcomics/ApplicationTest.java
@@ -1,13 +1,45 @@
 package com.robertoallende.marvelcomics;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import com.robertoallende.marvelcomics.view.ComicListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.anything;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+    @Rule
+    public ActivityTestRule<ComicListActivity> mActivityRule = new ActivityTestRule<>(
+            ComicListActivity.class);
+
+    @Test
+    public void navigateApp() {
+
+        onView(withId(R.id.recycler_view_comic_list)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        Espresso.pressBack();
+
     }
 }
